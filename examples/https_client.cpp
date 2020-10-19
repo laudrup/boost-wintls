052007415b4e5d8854321857c6cfc6fb596e7523
Kasper Laudrup <laudrup@stacktrace.dk>
2020-10-19 22:17:31 +0200

    Use async https example from boost::beast
    Instead of trying to implement a not really functional HTTPS client
    using plain boost::asio use a real HTTPS client by slightly modifying
    the existing example from the boost::beast example code.

--- examples/https_client.cpp
+++ examples/https_client.cpp
@@ -1,148 +1,192 @@
-#include <boost/asio.hpp>
+//
+// Copyright (c) 2016-2019 Vinnie Falco (vinnie dot falco at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//
+
+//------------------------------------------------------------------------------
+//
+// Example: HTTP SSL client, asynchronous
+//
+// Slightly modified from the example found at:
+// https://github.com/boostorg/beast
+// to use boost::windows_sspi instead of boost::asio::ssl when
+// building/running on Windows as well as using the operating systems'
+// default CAs.
+//
+//------------------------------------------------------------------------------
+
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
 
 #ifdef _WIN32
 #include <boost/windows_sspi/windows_sspi.hpp>
 #else
-#include <boost/asio/ssl.hpp>
+#include <boost/beast/ssl.hpp>
 #endif
 
 #include <cstdlib>
-#include <fstream>
+#include <functional>
 #include <iostream>
-#include <iterator>
+#include <memory>
 #include <string>
 
-namespace net = boost::asio;
+namespace beast = boost::beast;      // from <boost/beast.hpp>
+namespace http = beast::http;        // from <boost/beast/http.hpp>
+namespace net = boost::asio;         // from <boost/asio.hpp>
 
 #ifdef _WIN32
-namespace ssl = boost::windows_sspi;
+namespace ssl = boost::windows_sspi; // from <boost/windows_sspi/windows_sspi.hpp>
 #else
-namespace ssl = boost::asio::ssl;
+namespace ssl = boost::asio::ssl;    // from <boost/asio/ssl.hpp>
 #endif
 
-class https_client {
+using tcp = boost::asio::ip::tcp;    // from <boost/asio/ip/tcp.hpp>
+
+//------------------------------------------------------------------------------
+
+// Report a failure
+void fail(beast::error_code ec, char const* what) {
+  std::cerr << what << ": " << ec.message() << "\n";
+}
+
+// Performs an HTTP GET and prints the response
+class session : public std::enable_shared_from_this<session> {
+  tcp::resolver resolver_;
+  ssl::stream<beast::tcp_stream> stream_;
+  beast::flat_buffer buffer_; // (Must persist between reads)
+  http::request<http::empty_body> req_;
+  http::response<http::string_body> res_;
+
 public:
-  https_client(const std::string& host, const std::string& path, net::io_context& io_ctx, std::ostream* output)
-      : io_ctx_(io_ctx)
-      , ssl_ctx_(ssl::context::sslv23)
-      , socket_(io_ctx_, ssl_ctx_)
-      , output_(output) {
-
-    net::ip::tcp::resolver resolver(io_ctx);
-    net::ip::tcp::resolver::query query(host, "https");
-
-    // TODO: Consider async resolve
-    net::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
-    // TODO: Consider async connect
-    net::connect(socket_.next_layer(), endpoint_iterator);
-
-    socket_.async_handshake(ssl::stream_base::client, [this, &path, &host](const boost::system::error_code& ec) {
-      if (ec) {
-        std::cerr << "Error handshaking:" << ec.message() << "\n";
-        return;
-      }
-      std::ostream request_stream(&request_);
-      request_stream << "GET " << path << " HTTP/1.0\r\n";
-      request_stream << "Host: " << host << "\r\n";
-      request_stream << "Accept: */*\r\n\r\n";
-
-      boost::asio::async_write(socket_, request_, [this](const boost::system::error_code& ec, std::size_t) {
-        if (ec) {
-          std::cerr << "Error sending request:" << ec.message() << "\n";
-          return;
-        }
-        net::async_read_until(socket_, response_, "\r\n", [this](boost::system::error_code ec, size_t) {
-          if (ec) {
-            std::cerr << "Error receiving response: " << ec.message() << "\n";
-            return;
-          }
-          read_response();
-        });
-      });
-    });
+  explicit session(net::executor ex, ssl::context& ctx)
+      : resolver_(ex)
+      , stream_(ex, ctx) {
   }
 
-private:
-  void read_response() {
-    std::istream response_stream(&response_);
-    std::string http_version;
-    response_stream >> http_version;
-    unsigned int status_code;
-    response_stream >> status_code;
-    std::string status_message;
-    std::getline(response_stream, status_message);
-    if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
-      std::cerr << "Invalid response\n";
-      return;
+  // Start the asynchronous operation
+  void run(char const* host, char const* port, char const* target, int version) {
+
+    // Set up an HTTP GET request message
+    req_.version(version);
+    req_.method(http::verb::get);
+    req_.target(target);
+    req_.set(http::field::host, host);
+    req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+    // Look up the domain name
+    resolver_.async_resolve(host, port, beast::bind_front_handler(&session::on_resolve, shared_from_this()));
+  }
+
+  void on_resolve(beast::error_code ec, tcp::resolver::results_type results) {
+    if (ec)
+      return fail(ec, "resolve");
+
+    // Set a timeout on the operation
+    beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+
+    // Make the connection on the IP address we get from a lookup
+    beast::get_lowest_layer(stream_).async_connect(results, beast::bind_front_handler(&session::on_connect, shared_from_this()));
+  }
+
+  void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type) {
+    if (ec)
+      return fail(ec, "connect");
+
+    // Perform the SSL handshake
+    stream_.async_handshake(ssl::stream_base::client, beast::bind_front_handler(&session::on_handshake, shared_from_this()));
+  }
+
+  void on_handshake(beast::error_code ec) {
+    if (ec)
+      return fail(ec, "handshake");
+
+    // Set a timeout on the operation
+    beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+
+    // Send the HTTP request to the remote host
+    http::async_write(stream_, req_, beast::bind_front_handler(&session::on_write, shared_from_this()));
+  }
+
+  void on_write(beast::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+
+    if (ec)
+      return fail(ec, "write");
+
+    // Receive the HTTP response
+    http::async_read(stream_, buffer_, res_, beast::bind_front_handler(&session::on_read, shared_from_this()));
+  }
+
+  void on_read(beast::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+
+    if (ec)
+      return fail(ec, "read");
+
+    // Write the message to standard out
+    std::cout << res_ << std::endl;
+
+    // Set a timeout on the operation
+    beast::get_lowest_layer(stream_).expires_after(std::chrono::seconds(30));
+
+    // Gracefully close the stream
+    stream_.async_shutdown(beast::bind_front_handler(&session::on_shutdown, shared_from_this()));
+  }
+
+  void on_shutdown(beast::error_code ec) {
+    if (ec == net::error::eof) {
+      // Rationale:
+      // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
+      ec = {};
     }
-    if (status_code != 200) {
-      std::cerr << "Response returned with status code " << status_code << "\n";
-      return;
-    }
-    net::async_read_until(socket_, response_, "\r\n\r\n", [this](boost::system::error_code ec, size_t) {
-      if (ec) {
-        std::cerr << "Error reading headers: " << ec.message() << "\n";
-        return;
-      }
-      read_headers();
-    });
-  }
+    if (ec)
+      return fail(ec, "shutdown");
 
-  void read_headers() {
-    std::istream response_stream(&response_);
-    std::string header;
-    while (std::getline(response_stream, header) && header != "\r") {
-      std::cout << header << "\n";
-    }
-    std::cout << "\n";
-    read_body();
+    // If we get here then the connection is closed gracefully
   }
-
-  void read_body() {
-    async_read(socket_, response_, net::transfer_at_least(1), [this](boost::system::error_code ec, size_t) {
-      *output_ << &response_;
-      if (!ec) {
-        read_body();
-      }
-    });
-  }
-
-  net::io_context& io_ctx_;
-  ssl::context ssl_ctx_;
-  ssl::stream<net::ip::tcp::socket> socket_;
-  net::streambuf request_;
-  net::streambuf response_;
-  std::ostream* output_;
 };
 
-int main(int argc, char* argv[]) {
-  if (argc != 2 && argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " <url> [file]\n";
-    std::cerr << "Example:\n";
-    std::cerr << "  " << argv[0] << " https://www.boost.org/LICENSE_1_0.txt license.txt\n";
+//------------------------------------------------------------------------------
+
+int main(int argc, char** argv) {
+  // Check command line arguments.
+  if (argc != 4 && argc != 5) {
+    std::cerr << "Usage: http-client-async-ssl <host> <port> <target> [<HTTP version: 1.0 or 1.1(default)>]\n"
+              << "Example:\n"
+              << "    http-client-async-ssl www.example.com 443 /\n"
+              << "    http-client-async-ssl www.example.com 443 / 1.0\n";
     return EXIT_FAILURE;
   }
+  auto const host = argv[1];
+  auto const port = argv[2];
+  auto const target = argv[3];
+  int version = argc == 5 && !std::strcmp("1.0", argv[4]) ? 10 : 11;
 
-  std::string url(argv[1]);
-  if (url.rfind("https://", 0) != 0) {
-    std::cerr << "Unsupported url: " << url << "\n";
-    return EXIT_FAILURE;
-  }
-  const std::string host = url.substr(8, url.find('/', 8) - 8);
-  const std::string path = url.find('/', 8) == std::string::npos ? "/" : url.substr(url.find('/', 8));
+  // The io_context is required for all I/O
+  net::io_context ioc;
 
-  std::ostream* ofs;
-  std::ofstream out_file;
-  if (argc == 3) {
-    out_file = std::ofstream(argv[2], std::ios::out | std::ios::binary);
-    ofs = &out_file;
-  } else {
-    ofs = &std::cout;
-  }
+  // The SSL context is required, and holds certificates
+  ssl::context ctx{ssl::context::tlsv12_client};
 
-  net::io_context io_ctx;
-  https_client client{host, path, io_ctx, ofs};
-  io_ctx.run();
+  // Use the operating systems default certficates for verification
+  ctx.set_default_verify_paths();
+
+  // Verify the remote server's certificate
+  ctx.set_verify_mode(ssl::verify_peer);
+
+  // Launch the asynchronous operation
+  // The session is constructed with a strand to
+  // ensure that handlers do not execute concurrently.
+  std::make_shared<session>(net::make_strand(ioc), ctx)->run(host, port, target, version);
+
+  // Run the I/O service. The call will return when
+  // the get operation is complete.
+  ioc.run();
+
   return EXIT_SUCCESS;
 }

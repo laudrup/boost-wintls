bce90cd76011a5d5121b794b7b83797fd57d0364
Kasper Laudrup <laudrup@stacktrace.dk>
2021-07-02 00:53:26 +0200

    Simplify shutdown handling
    No reason to return states from the shutdown implementation when
    shutting down a TLS connection is only about creating a shutdown token
    that needs to be sent to the peer so return that as a buffer instead,
    but still tell the client if creating the token went wrong for some
    reason.

--- include/boost/wintls/detail/sspi_shutdown.hpp
+++ include/boost/wintls/detail/sspi_shutdown.hpp
@@ -20,58 +20,47 @@ namespace detail {
 
 class sspi_shutdown {
 public:
-  enum class state {
-    data_available,
-    error
-  };
-
   sspi_shutdown(CtxtHandle* context, CredHandle* credentials)
     : context_(context)
-    , credentials_(credentials)
-    , last_error_(SEC_E_OK) {
+    , credentials_(credentials) {
   }
 
-  state operator()() {
+  boost::system::error_code operator()() {
     shutdown_buffers buffers;
 
-    last_error_ = detail::sspi_functions::ApplyControlToken(context_, buffers);
-    if (last_error_ != SEC_E_OK) {
-      return state::error;
+    SECURITY_STATUS sc = detail::sspi_functions::ApplyControlToken(context_, buffers);
+    if (sc != SEC_E_OK) {
+      return error::make_error_code(sc);
     }
 
     DWORD out_flags = 0;
-    last_error_ = detail::sspi_functions::InitializeSecurityContext(credentials_,
-                                                                    context_,
-                                                                    nullptr,
-                                                                    client_context_flags,
-                                                                    0,
-                                                                    SECURITY_NATIVE_DREP,
-                                                                    nullptr,
-                                                                    0,
-                                                                    context_,
-                                                                    buffers,
-                                                                    &out_flags,
-                                                                    nullptr);
-    if (last_error_ != SEC_E_OK) {
-      return state::error;
+    sc = detail::sspi_functions::InitializeSecurityContext(credentials_,
+                                                           context_,
+                                                           nullptr,
+                                                           client_context_flags,
+                                                           0,
+                                                           SECURITY_NATIVE_DREP,
+                                                           nullptr,
+                                                           0,
+                                                           context_,
+                                                           buffers,
+                                                           &out_flags,
+                                                           nullptr);
+    if (sc != SEC_E_OK) {
+      return error::make_error_code(sc);
     }
 
-    buffer_ = std::move(sspi_context_buffer{buffers[0].pvBuffer, buffers[0].cbBuffer});
-    return state::data_available;
+    buffer_ = sspi_context_buffer{buffers[0].pvBuffer, buffers[0].cbBuffer};
+    return {};
   }
 
-  net::const_buffer output() const {
-    return buffer_;
-  }
-
-  boost::system::error_code last_error() const {
-    return error::make_error_code(last_error_);
+  net::const_buffer buffer() {
+    return buffer_.asio_buffer();
   }
 
 private:
   CtxtHandle* context_;
   CredHandle* credentials_;
-  SECURITY_STATUS last_error_;
   sspi_context_buffer buffer_;
 };
 

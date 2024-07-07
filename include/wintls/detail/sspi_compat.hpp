72a50e8810c181e16894544926affc731f167d49
windowsair <msdn_02@sina.com>
2024-07-07 20:20:00 +0800

    Add support for TLS v1.3
    TLS v1.3 is available on windows 10.0.17763 and above version.

---
+++ include/wintls/detail/sspi_compat.hpp
@@ -0,0 +1,64 @@
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef WINTLS_DETAIL_SSPI_COMPAT_HPP
+#define WINTLS_DETAIL_SSPI_COMPAT_HPP
+
+// for SCH_CREDENTIALS
+#define SCHANNEL_USE_BLACKLISTS
+
+#include <sdkddkver.h>
+#include <SubAuth.h>
+#include <schannel.h>
+
+#if (WDK_NTDDI_VERSION < NTDDI_WIN10_19H1)
+typedef enum _eTlsAlgorithmUsage
+{
+    TlsParametersCngAlgUsageKeyExchange,
+    TlsParametersCngAlgUsageSignature,
+    TlsParametersCngAlgUsageCipher,
+    TlsParametersCngAlgUsageDigest,
+    TlsParametersCngAlgUsageCertSig
+} eTlsAlgorithmUsage;
+
+typedef struct _CRYPTO_SETTINGS
+{
+    eTlsAlgorithmUsage  eAlgorithmUsage;
+    UNICODE_STRING      strCngAlgId;
+    DWORD               cChainingModes;
+    PUNICODE_STRING     rgstrChainingModes;
+    DWORD               dwMinBitLength;
+    DWORD               dwMaxBitLength;
+} CRYPTO_SETTINGS, * PCRYPTO_SETTINGS;
+
+typedef struct _TLS_PARAMETERS
+{
+    DWORD               cAlpnIds;
+    PUNICODE_STRING     rgstrAlpnIds;
+    DWORD               grbitDisabledProtocols;
+    DWORD               cDisabledCrypto;
+    PCRYPTO_SETTINGS    pDisabledCrypto;
+    DWORD               dwFlags;
+} TLS_PARAMETERS, * PTLS_PARAMETERS;
+
+typedef struct _SCH_CREDENTIALS
+{
+    DWORD               dwVersion;
+    DWORD               dwCredFormat;
+    DWORD               cCreds;
+    PCCERT_CONTEXT* paCred;
+    HCERTSTORE          hRootStore;
+
+    DWORD               cMappers;
+    struct _HMAPPER **aphMappers;
+
+    DWORD               dwSessionLifespan;
+    DWORD               dwFlags;
+    DWORD               cTlsParameters;
+    PTLS_PARAMETERS     pTlsParameters;
+} SCH_CREDENTIALS, * PSCH_CREDENTIALS;
+#endif // (NTDDI_VERSION < NTDDI_WIN10_19H1)
+
+#endif // WINTLS_DETAIL_SSPI_COMPAT_HPP
